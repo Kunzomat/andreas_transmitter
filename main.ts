9b43a146996015e33d22d807632b4d1b0f08c635
Andreas Brisgen <akunze_de@web.de>
2020-09-08 14:32:59 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+WSJoyStick.onKey(KEY.P, function () {
+    radio.sendString("P")
+    basic.showString("P")
+})
 WSJoyStick.JoyStickInit()
 radio.setGroup(1)
 basic.showLeds(`
@@ -9,51 +13,31 @@ basic.showLeds(`
     `)
 basic.forever(function () {
     if (WSJoyStick.Listen_Dir(DIR.U)) {
-        radio.sendNumber(0)
+        radio.sendNumber(1)
         basic.showArrow(ArrowNames.North)
     } else if (WSJoyStick.Listen_Dir(DIR.U_R)) {
-        radio.sendNumber(1)
+        radio.sendNumber(2)
         basic.showArrow(ArrowNames.NorthEast)
     } else if (WSJoyStick.Listen_Dir(DIR.R)) {
-        radio.sendNumber(2)
+        radio.sendNumber(3)
         basic.showArrow(ArrowNames.East)
     } else if (WSJoyStick.Listen_Dir(DIR.D_R)) {
-        radio.sendNumber(3)
+        radio.sendNumber(4)
         basic.showArrow(ArrowNames.SouthEast)
     } else if (WSJoyStick.Listen_Dir(DIR.D)) {
-        radio.sendNumber(4)
+        radio.sendNumber(5)
         basic.showArrow(ArrowNames.South)
     } else if (WSJoyStick.Listen_Dir(DIR.D_L)) {
-        radio.sendNumber(5)
+        radio.sendNumber(6)
         basic.showArrow(ArrowNames.SouthWest)
     } else if (WSJoyStick.Listen_Dir(DIR.L)) {
-        radio.sendNumber(6)
+        radio.sendNumber(7)
         basic.showArrow(ArrowNames.West)
     } else if (WSJoyStick.Listen_Dir(DIR.U_L)) {
-        radio.sendNumber(7)
-        basic.showArrow(ArrowNames.NorthWest)
-    } else if (WSJoyStick.Listen_Key(KEY.P)) {
         radio.sendNumber(8)
-        basic.showString("P")
-    } else if (WSJoyStick.Listen_Key(KEY.A)) {
-        radio.sendNumber(9)
-        basic.showString("A")
-    } else if (WSJoyStick.Listen_Key(KEY.B)) {
-        radio.sendNumber(10)
-        basic.showString("B")
-    } else if (WSJoyStick.Listen_Key(KEY.C)) {
-        radio.sendNumber(11)
-        basic.showString("C")
-    } else if (WSJoyStick.Listen_Key(KEY.D)) {
-        radio.sendNumber(12)
-        basic.showString("D")
-    } else if (WSJoyStick.Listen_Key(KEY.E)) {
-        radio.sendNumber(13)
-        basic.showString("E")
-    } else if (WSJoyStick.Listen_Key(KEY.F)) {
-        radio.sendNumber(14)
-        basic.showString("F")
+        basic.showArrow(ArrowNames.NorthWest)
     } else {
+        radio.sendNumber(0)
         basic.showLeds(`
             . . . . .
             . . . . .
